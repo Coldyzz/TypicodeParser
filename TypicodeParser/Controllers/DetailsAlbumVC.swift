c237ab065b72ffa4d49025fc5915dd8005fe97cb
Coldyzz <dkgkpizza@gmail.com>
2022-10-26 19:11:14 +0200

    added

--- TypicodeParser/Controllers/DetailsAlbumVC.swift
+++ TypicodeParser/Controllers/DetailsAlbumVC.swift
@@ -7,11 +7,23 @@
 
 import UIKit
 
-class DetailsAlbumVC: UIViewController {
+class DetailsAlbumVC: UIViewController, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
     @IBOutlet weak var detailsView: UICollectionView!
     let detailListRepo = DetailListRepository()
     var detailList: [DetailAlbumList] = []
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "DETAILS ALBUM"
+        let cellNib = UINib(nibName: "DetailAlbumCell", bundle: nil)
+        detailsView.dataSource = self
+        detailsView.register(cellNib, forCellWithReuseIdentifier: "DetailAlbumCell")
     }
 }
